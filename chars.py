38c296488007d286e6cd9d7cc377a4e16ab539c7
derpford <cascott1994@gmail.com>
2018-03-14 12:30:44 -0500

    Fixed issue where getSides modifies sides.

--- chars.py
+++ chars.py
@@ -6,6 +6,7 @@ from random import *
 # Valid symbols to add to a die.
 class Symbol(Enum):
     # BASIC TYPES
+    BLANK=0;#Blank symbol.
     GUN=1;#Ranged.
     MELEE=2;#Melee.
     SCI=3;#High-tech stuff.
@@ -29,7 +30,6 @@ class Dice():
             if isinstance(k,Symbol):
                 self.sides.append(k);
             else:
-                print(type(k));
                 raise ValueError("Tried to add a non-symbol to a die: "+str(k)+" of type "+str(type(k)));
         for i in range(len(self.sides)):
             print(i)
@@ -39,19 +39,25 @@ class Dice():
         """Returns a side from the die. No need to check side count. Also
         ensures that a modified side is returned, if any."""
         result = randrange(len(self.sides));
+        resym = None; # The symbol from the result.
         try:
             if self.modsides[result] != None:
-                return self.modsides[result];
+                resym = self.modsides[result];
             else:
-                return self.sides[result];
+                resym = self.sides[result];
         except IndexError:
-            return self.sides[result];
+            resym = self.sides[result];
+        # Gonna need to add something that handles the result.
+        if resym is Symbol.STUN:
+            self.modsides[result] = None;
+        return resym;
+
 
     def modify(self,replace):
         """Replaces a random side with a modifier 'replace'. Only picks sides that aren't already modified."""
         done=False;
         if not isinstance(replace, Symbol):
-            raise ValueError("Tried to replace a die face with a non-symbol value!");
+            raise ValueError("Tried to replace a die face with a non-symbol value:"+str(replace));
         while not done:
             result = randrange(len(self.sides));
             try:
@@ -64,7 +70,8 @@ class Dice():
 
     def getSides(self):
         """Returns a table with either the Sides or ModSides value for each face of the die."""
-        sidelist = self.sides;
+        sidelist = [];
+        sidelist.extend(self.sides); # Make sidelist a copy of sides, without modifying original sides.
         for i in range(len(self.sides)):
             try:
                 if self.modsides[i] != None:
@@ -78,4 +85,15 @@ if __name__ == "__main__": # Time to test dice.
     print(die.roll(),"Roll");
     die.modify(Symbol.STUN);
     print("Die modified with STUN");
-    print(die.getSides());
+    print("getSides call: ",die.getSides());
+    print("Repeated rolling!")
+    for i in range (1,12):
+        print("Real sides: ",die.sides);
+        print("Modifiers: ",die.modsides);
+        print("Result: ",die.roll());
+    print("Stunned Roll!")
+    for i in range(1,6):
+        die.modify(Symbol.STUN);
+    for i in range(1,12):
+        print(die.getSides());
+        print("Result: ",die.roll());
