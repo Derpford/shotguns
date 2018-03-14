3828ab3dfaa9ad8f3a57125aa24287a03748db85
derpford <cascott1994@gmail.com>
2018-03-14 11:16:52 -0500

    Added modsides.

--- chars.py
+++ chars.py
@@ -2,20 +2,32 @@ from enum import Enum
 
 #For characters.
 class Symbol(Enum):
-    GUN=1;
-    MELEE=2;
-    SCI=3;
-    CULT=4;
-    MOVE=5;
-    DEF=6;
+    # BASIC TYPES
+    GUN=1;#Ranged.
+    MELEE=2;#Melee.
+    SCI=3;#High-tech stuff.
+    CULT=4;#Magic stuff.
+    MOVE=5;#Tactical movement.
+    DEF=6;#Riot shields, etc
+    # DIE MODIFIERS
+    STUN=7;#Goes away when rolled.
+    WOUND=8;#Only removed by medical treatments.
+    BURN=9;#When rolled, randomly moves to another die.
+    SICK=10;#When rolled, randomly moves to another face of the die.
 
 class Dice():
     sides={}
+    modsides={}
     def __init__(self):
         """Each character has a die associated with it. This die represents
         the character's abilities and health. Faces of the die may be "covered"
         when affected by damage."""
 
     def roll(self):
-        """Returns a side from the die. No need to check side count."""
-        return sides[random(len(sides))];
+        """Returns a side from the die. No need to check side count. Also
+        ensures that a modified side is returned, if any."""
+        result = random(len(sides));
+        if modsides[result] != None:
+            return modsides[result];
+        else:
+            return sides[result];
